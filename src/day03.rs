a01d621db0b8f5c602c67c5c2444ddec600c5d10
Josh Lartz <josh.lartz@gmail.com>
2024-12-08 13:26:14 -0600

    day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -4,41 +4,61 @@ use regex::Regex;
 type Input<'a> = Vec<&'a str>;
 
 pub fn generator(input: &str) -> Input {
-    let re = Regex::new(r"(?m)mul\(\d+,\d+\)").unwrap();
+    let re = Regex::new(r"(?m)mul\(\d+,\d+\)|don't\(\)|do\(\)").unwrap();
 
     re.find_iter(input).map(|m| m.as_str()).collect_vec()
 }
 
 pub fn part1(input: &Input) -> usize {
-    let re = Regex::new(r"(\d+),(\d+)").unwrap();
+    input
+        .iter()
+        .filter(|each| each.contains("mul"))
+        .map(multiply)
+        .sum()
+}
+
+pub fn part2(input: &Input) -> usize {
+    let mut enabled = true;
 
     input
         .iter()
         .map(|each| {
-            let caps = re.captures(each).unwrap();
-            caps.get(1).unwrap().as_str().parse::<usize>().unwrap()
-                * caps.get(2).unwrap().as_str().parse::<usize>().unwrap()
+            if each == &"do()" {
+                enabled = true;
+            }
+            if each == &"don't()" {
+                enabled = false;
+            }
+            if each.contains("mul") && enabled {
+                return multiply(each);
+            }
+            0
         })
         .sum()
 }
 
-// pub fn part2(input: &Input) -> usize {
+fn multiply(each: &&str) -> usize {
+    let re = Regex::new(r"(\d+),(\d+)").unwrap();
 
-// }
+    let caps = re.captures(each).unwrap();
+    caps.get(1).unwrap().as_str().parse::<usize>().unwrap()
+        * caps.get(2).unwrap().as_str().parse::<usize>().unwrap()
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const SAMPLE: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const SAMPLE: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn test_part1() {
         assert_eq!(part1(&generator(SAMPLE)), 161);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(&generator(SAMPLE)), 4);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&generator(SAMPLE)), 48);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ aoc_main::main! {
   year 2024;
   day01 : generator => part1, part2;
   day02 : generator => part1, part2;
-  day03 : generator => part1;
+  day03 : generator => part1, part2;
 }
